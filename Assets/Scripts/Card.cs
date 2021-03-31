093bb319dddecc3e08746d66b0aa61896327995a
omtechgames@gmail.com <utythfnjhubn>
2021-03-31 20:35:18 +0300

    добавлены исправления

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,20 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
     public int color;
     public bool unable = false;
-     
+
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
     public void Go()
     {
         unable = true;
+        button.enabled = false;
         transform.parent.GetComponent<Hand>().ReSort();
         S.score++;
         LeanTween.move(gameObject, new Vector3(Screen.width / 2, Screen.height / 2), 0.5f).setEaseOutCirc();
         LeanTween.scale(gameObject, Vector3.zero, 0.2f).setDelay(1.8f).setEaseOutCirc();
-        StartCoroutine("Disabl");
+        StartCoroutine(Disabl());
     }
     IEnumerator Disabl()
     {
